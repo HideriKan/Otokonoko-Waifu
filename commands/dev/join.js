d73fac234e878bccd351a7ba875f012c9503dd97
keinschimer <keinschimermax@gmail.com>
2018-07-31 15:59:41 +0200

    Baka

--- commands/dev/join.js
+++ commands/dev/join.js
@@ -5,27 +5,6 @@ module.exports = class xCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: "join",
-<<<<<<< HEAD
-			memberName: "join",
-			group: "dev",
-			description: "joins a channel",
-			throttling: {
-				usages: 1, // in the time frame
-				duration: 5 // in seconds
-			},
-			aliases: ["j"],
-			examples: ["join", "j"],
-			details: "Attempts to join this voice channel",
-		});
-	}
-
-	run(msg) {
-		const vc = msg.guild.channels.find("id","430767868125118468");
-		console.log();
-		vc.join();
-	}
-};
-=======
 			memberName: "j",
 			group: "dev",
 			description: "Join the Channel",
@@ -41,4 +20,3 @@ module.exports = class xCommand extends Command {
 		msg.member.voiceChannel.join();
 	}
 };
->>>>>>> ae7dea75ee52276d8a79e307adb735319c10d8b9
