287e6827a6fa536d078d9c713c27cd91891fad46
keinschimer <keinschimermax@gmail.com>
2018-09-14 22:47:28 +0200

    updated suggest

--- commands/usefull/suggest.js
+++ commands/usefull/suggest.js
@@ -11,21 +11,20 @@ module.exports = class SuggestCommand extends Command {
 				usages: 2, // in the time frame
 				duration: 60 // in seconds
 			},
-			aliases: ["report", "bug", "issue"],
+			aliases: ["report", "bug", "issue", "note"],
 			details: "Sends the message written after the command to me",
 		});
 	}
 
-	async run(msg) {
+	async run(msg ,args) {
 		const suggestCh = msg.client.channels.get("485069817427263488");
 		try {
 			if (msg.guild && msg.guild.members.some(e => !msg.client.owners.includes(e))) {
-				await suggestCh.send(`https://discordapp.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}
-				${msg.command}: ${msg.argString.trim()}`);
+				let usage = msg.content.match(/(?<=,,)\w*/g);
+				await suggestCh.send(`https://discordapp.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}\n${usage[0]}: ${args.trim()}`);
 
 			} else {
-				await suggestCh.send(`${msg.guild ? `Guild:${msg.guild.name}(${msg.guild.id}) Channel:<#${msg.channel.id}>` : msg.channel.type} by <@${msg.author.id}>:
-				${msg.command}: ${msg.argString.trim()}`);
+				await suggestCh.send(`${msg.guild ? `Guild:${msg.guild.name}(${msg.guild.id}) Channel:<#${msg.channel.id}>` : msg.channel.type} by <@${msg.author.id}>:\n${msg.command}: ${args.trim()}`);
 			}
 
 			return msg.channel.send("message was sent uwu");
