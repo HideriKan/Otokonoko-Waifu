1e84f7c050b6a2165797c66020331d92f5306def
keinschimer <keinschimermax@gmail.com>
2018-11-04 16:08:02 +0100

    yet again time fix

--- waifu.js
+++ waifu.js
@@ -75,14 +75,14 @@ function getNextResetDateInMs() {
 
 	switch (hour % 3) {
 	case 0:
-		if (now.getUTCMinutes() < 4) break;
-		hour += 3;
-		break;
-	case 1:
 		hour += 2;
 		break;
-	case 2:
+	case 1:
 		hour += 1;
+		break;
+	case 2:
+		if (now.getUTCMinutes() < 4) break;
+		hour += 3;
 		break;
 	}
 
